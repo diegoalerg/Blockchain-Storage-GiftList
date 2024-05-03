8006d20a95db19b08539ca026428ec1de03d22d4
diegoalerg <diegorodriguezgalindez@gmail.com>
2024-05-03 22:02:30 +0200

    Project #2 completed

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
-const express = require('express');
-const verifyProof = require('../utils/verifyProof');
+const express = require("express");
+const verifyProof = require("../utils/verifyProof");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
 const port = 1225;
 
@@ -7,20 +9,30 @@ const app = express();
 app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
-// paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+// create the merkle tree for the whole nice list
+const merkleTree = new MerkleTree(niceList);
 
-app.post('/gift', (req, res) => {
+// get the root
+const root = merkleTree.getRoot();
+
+const MERKLE_ROOT = root;
+
+app.post("/gift", (req, res) => {
   // grab the parameters from the front-end here
-  const body = req.body;
+  const { name } = req.body;
+  // find the proof that norman block is in the list
+  const index = niceList.findIndex((n) => n === name);
+  const proof = merkleTree.getProof(index);
 
-  // TODO: prove that a name is in the list 
-  const isInTheList = false;
-  if(isInTheList) {
+  // verify proof against the Merkle Root
+  console.log(verifyProof(proof, name, root));
+
+  // TODO: prove that a name is in the list
+
+  if (verifyProof(proof, name, root)) {
     res.send("You got a toy robot!");
-  }
-  else {
-    res.send("You are not on the list :(");
+  } else {
+    res.send("You are no on the list :(");
   }
 });
 
